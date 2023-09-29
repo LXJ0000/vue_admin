d0e4dcf2f6fe77f1f7feab320c7f9273cc848a91
XIAOJIANG LIN <1227891082@qq.com>
2023-09-29 21:56:56 +0800

    配置后端API

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    proxy:{
-      "/static":{
-        target:"http://127.0.0.1:8080/"
+
+export default ({mode}) =>{
+  const env = loadEnv(mode, process.cwd())
+  const baseUrl = env.VITE_API
+  return defineConfig({
+    envPrefix:["VITE_"],
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+    },
+    server:{
+      proxy:{
+        "/static":{
+          target:baseUrl
+        }
 
+      }
     }
-  }
 
-})
+  })
+
+}
+
+
+// https://vitejs.dev/config/
