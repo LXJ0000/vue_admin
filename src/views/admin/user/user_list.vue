0babf0c7f4f191aa492c060e8d9fe060a5b4f00b
XIAOJIANG LIN <1227891082@qq.com>
2023-10-01 14:28:35 +0800

    用户列表完成

--- src/views/admin/user/user_list.vue
+++ src/views/admin/user/user_list.vue
@@ -4,6 +4,7 @@ import {SmileOutlined} from '@ant-design/icons-vue';
 import {formatDateTime} from "@/utils/time";
 import {userCreateApi, userListApi, UserRemoveBatchApi, UserUpdateNickNameAndRoleApi} from "@/api/user";
 import {message} from "ant-design-vue";
+import {SyncOutlined} from '@ant-design/icons-vue';
 
 
 const data = reactive({
@@ -19,37 +20,22 @@ const data = reactive({
     {title: '注册时间', dataIndex: 'created_at', key: 'created_at'},
     {title: '操作', dataIndex: 'action', key: 'action'},
 
-  ],
-  dataSource: [],
-  cnt: 0,
-  modalCreateVisible: false,
-  modalUpdateVisible: false,
-
+  ], //列名
+  dataSource: [], //行内数据
+  cnt: 0, // 数据量
+  modalCreateVisible: false, // 弹框
+  modalUpdateVisible: false, // 弹框
+  spinning: true, //加载ing
 })
 
-const _formState = {
-  nick_name: "",
-  user_name: "",
-  password: "",
-  rePassword: "",
-  role: "",
-}
-const formState = reactive({
-  nick_name: "",
-  user_name: "",
-  password: "",
-  rePassword: "",
-  role: "",
-})
 
+// 选择数据后可以批量删除
 const state = reactive({
-  selectedRowKeys: [],
+  selectedRowKeys: [], // 选择的数据 id
   // Check here to configure the default column
   loading: false,
 });
-
 const hasSelected = computed(() => state.selectedRowKeys.length > 0);
-
 const start = () => {
   state.loading = true;
   // ajax request after empty completing
@@ -58,11 +44,11 @@ const start = () => {
     state.selectedRowKeys = [];
   }, 1000);
 };
-
 const onSelectChange = selectedRowKeys => {
   state.selectedRowKeys = selectedRowKeys;
 };
 
+// 批量删除
 const removeBatch = async () => {
   let res = await UserRemoveBatchApi(state.selectedRowKeys)
   if (res.code) {
@@ -74,39 +60,56 @@ const removeBatch = async () => {
   state.selectedRowKeys = []
 }
 
+
+// 分页
 const page = reactive({
   page: 1,
-  limit: 5
+  limit: 5,
+  key: "",
+  role: undefined,
 })
+const pageChange = (_page, _limit) => {
+  GetData()
+}
 
+// 获取数据
 async function GetData() {
+  data.spinning = true
+
   let res = await userListApi(page)
   data.dataSource = res.data.list
   data.cnt = res.data.count
+
+  data.spinning = false
+
 }
 
 GetData()
 
-
-const options = [
-  {
-    value: 1, label: "管理员"
-  },
-  {
-    value: 2, label: "普通用户"
-  },
-  {
-    value: 3, label: "游客"
-  },
-
-]
-
+// 添加用户表单数据
+const _formState = {
+  nick_name: "",
+  user_name: "",
+  password: "",
+  rePassword: "",
+  role: null,
+}
+const formState = reactive({
+  nick_name: "",
+  user_name: "",
+  password: "",
+  rePassword: "",
+  role: undefined,
+}) // 用户刷新弹框数据
 const formRef = ref(null)
+//添加用户弹框
+const addUserModal = () => {
+  data.modalCreateVisible = true
+}
 
+//添加用户
 async function handleOk() {
   try {
-    // let valid = await formRef.value.validate()
-    // // 登录请求
     let valid = await formRef.value.validate()
     let res = await userCreateApi(valid)
     if (res.code) {
@@ -122,13 +125,13 @@ async function handleOk() {
   }
 }
 
+// 自定义验证密码和确认密码
 const validatePass = async (_rule, value) => {
   if (value === '') {
     return Promise.reject('请输入密码');
   }
   return Promise.resolve();
 };
-
 const validatePass2 = async (_rule, value) => {
   if (value === '') {
     return Promise.reject('请再次输入密码');
@@ -139,10 +142,13 @@ const validatePass2 = async (_rule, value) => {
   }
 };
 
-const pageChange = (_page, _limit) => {
-  GetData()
-}
 
+const formUpdateState = reactive({
+  nick_name: "",
+  role: undefined,
+  user_id: ""
+})
+//更新用户数据弹框
 const updateUserInfoModal = record => {
   data.modalUpdateVisible = true
   formUpdateState.user_id = record.id
@@ -161,20 +167,39 @@ const UpdateUserInfo = async () => {
   data.modalUpdateVisible = false
 }
 
-const addUserModal = () => {
-  data.modalCreateVisible = true
+//权限选择下拉菜单
+const options = [
+  {
+    value: 1, label: "管理员"
+  },
+  {
+    value: 2, label: "普通用户"
+  },
+  {
+    value: 3, label: "游客"
+  },
+
+]
+
+//刷新
+const refresh = () => {
+  GetData()
+  message.success("刷新成功")
 }
 
-const formUpdateState = reactive({
-  nick_name: "",
-  role: "",
-  user_id: ""
-})
+const onSearch = () => {
+  page.page = 1
+  page.key = page.key.trim()
+  GetData()
+}
+
+
 
 </script>
 
 <template>
   <div class="container">
+    <!--    添加用户-->
     <a-modal
         v-model:open="data.modalCreateVisible"
         title="添加用户"
@@ -239,7 +264,7 @@ const formUpdateState = reactive({
 
       </a-form>
     </a-modal>
-
+    <!--编辑用户-->
     <a-modal
         v-model:open="data.modalUpdateVisible"
         title="编辑用户"
@@ -271,8 +296,8 @@ const formUpdateState = reactive({
             :rules="[{ required: true, message: '请选择权限!' }]"
         >
           <a-select
-              placeholder="权限"
               v-model:value="formUpdateState.role"
+              placeholder="权限"
               style="width: 200px"
               :options="options"
           ></a-select>
@@ -280,81 +305,107 @@ const formUpdateState = reactive({
 
       </a-form>
     </a-modal>
+    <!-- todo搜索框 -->
+    <div class="container_searchAndRefresh">
+      <div class="container_search">
+        <a-input-search
+            placeholder="搜索用户昵称"
+            style="width: 200px"
+            @search="onSearch"
+            v-model:value="page.key"
+        />
 
-    <div class="container_search">
-      <a-input-search
-          placeholder="搜索用户昵称"
-          style="width: 200px"
-      />
+        <a-select
+            @change="onSearch"
+            v-model:value="page.role"
+            style="width: 200px; padding-left: 10px"
+            :options="options"
+            placeholder="请筛选权限"
+            allowClear
+        ></a-select>
+      </div>
+      <div class="container_refresh">
+        <a-button @click="refresh">
+          <SyncOutlined/>
+        </a-button>
+      </div>
     </div>
 
+
+    <!--用户列表-->
     <div class="container_table">
+
 
       <div>
         <div class="container_table_action">
-          <a-button type="primary" :disabled="!hasSelected" :loading="state.loading" @click="start">
-            重置
-          </a-button>
-          <a-button type="primary" @click="addUserModal">添加</a-button>
+          <div class="container_table_action_left">
+            <a-button type="primary" :disabled="!hasSelected" :loading="state.loading" @click="start">
+              重置
+            </a-button>
+            <a-button type="primary" @click="addUserModal">添加</a-button>
 
-          <a-popconfirm
-              title="是否确定删除"
-              ok-text="删除"
-              cancel-text="取消"
-              @confirm="removeBatch"
-          >
-            <a-button type="primary" danger v-if="state.selectedRowKeys.length">批量删除</a-button>
-          </a-popconfirm>
+            <a-popconfirm
+                title="是否确定删除"
+                ok-text="删除"
+                cancel-text="取消"
+                @confirm="removeBatch"
+            >
+              <a-button type="primary" danger v-if="state.selectedRowKeys.length">批量删除</a-button>
+            </a-popconfirm>
+          </div>
+
+
+          <div class="container_table_action_right">
+
+          </div>
 
         </div>
 
-        <span style="margin-left: 8px">
-        <template v-if="hasSelected">
-          {{ `Selected ${state.selectedRowKeys.length} items` }}
-        </template>
-      </span>
       </div>
+      <a-spin :spinning="data.spinning" tip="加载中. . ." :delay="300">
+        <a-table
+            :row-selection="{ selectedRowKeys: state.selectedRowKeys, onChange: onSelectChange }"
+            :row-key="'id'"
 
-      <a-table
-          :row-selection="{ selectedRowKeys: state.selectedRowKeys, onChange: onSelectChange }"
-          :row-key="'id'"
+            :dataSource="data.dataSource"
+            :columns="data.columns"
 
-          :dataSource="data.dataSource"
-          :columns="data.columns"
+            :pagination="false"
 
-          :pagination="false"
-
-      >
-        <!--修改表头样式-->
-        <template #headerCell="{ column }">
-          <template v-if="column.key === 'nick_name'">
+        >
+          <!--修改表头样式-->
+          <template #headerCell="{ column }">
+            <template v-if="column.key === 'nick_name'">
         <span>
           <smile-outlined/>
           昵称
         </span>
+            </template>
           </template>
-        </template>
-        <!--修改表中样式-->
-        <template #bodyCell="{ column, record }">
-          <template v-if="column.key === 'avatar'">
-            <img class="table_avatar" :src="record.avatar" alt="">
-            <!--            代理！！！代理到目标地址-->
+          <!--修改表中样式-->
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.key === 'avatar'">
+              <img class="table_avatar" :src="record.avatar" alt="">
+              <!--            代理！！！代理到目标地址-->
+            </template>
+
+            <template v-if="column.key === 'created_at'">
+              <div>{{ formatDateTime(record.created_at) }}</div>
+            </template>
+
+            <template v-if="column.key === 'action'">
+              <a-button @click="updateUserInfoModal(record)" type="primary" ghost disabled
+                        v-if="record.role === '管理员'">
+                编辑
+              </a-button>
+              <a-button @click="updateUserInfoModal(record)" type="primary" ghost v-else>编辑</a-button>
+            </template>
           </template>
 
-          <template v-if="column.key === 'created_at'">
-            <div>{{ formatDateTime(record.created_at) }}</div>
-          </template>
+        </a-table>
 
-          <template v-if="column.key === 'action'">
-            <a-button @click="updateUserInfoModal(record)" type="primary" ghost disabled
-                      v-if="record.role === '管理员'">
-              编辑
-            </a-button>
-            <a-button @click="updateUserInfoModal(record)" type="primary" ghost v-else>编辑</a-button>
-          </template>
-        </template>
+      </a-spin>
 
-      </a-table>
       <div class="container_table_page flex">
         <a-pagination
             v-model:current="page.page"
@@ -378,9 +429,19 @@ const formUpdateState = reactive({
   background-color: var(--inbg);
   border-radius: 10px;
 
-  .container_search {
+  .container_searchAndRefresh {
     padding: 10px;
     border-bottom: 1px solid var(--outbg);
+    position: relative;
+
+
+    .container_refresh {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+
+    }
+
   }
 
 
@@ -388,10 +449,14 @@ const formUpdateState = reactive({
     padding: 10px;
 
     .container_table_action {
+      padding-bottom: 10px;
 
-      .ant-btn {
-        margin-right: 10px;
+      .container_table_action_left {
+        .ant-btn {
+          margin-right: 10px;
+        }
       }
+
     }
 
     .table_avatar {
